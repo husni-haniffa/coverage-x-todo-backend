6d46a40618fb2a16ef6e09998d67448609bb1b5b
Husni <husniwfayo@gmail.com>
2025-10-22 22:40:16 +0530

    initial commit

---
+++ src/main/java/com/coverage_x_todo_backend/coverage_x_todo_backend/CoverageXTodoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.coverage_x_todo_backend.coverage_x_todo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoverageXTodoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoverageXTodoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coverage_x_todo_backend/coverage_x_todo_backend/CoverageXTodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coverage_x_todo_backend.coverage_x_todo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoverageXTodoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
